1e059f29e9e6f4e06076ac45878d61db5eabee64
Imsha Nadeem <139887420+imshaa@users.noreply.github.com>
2025-08-24 21:11:54 +0500

    Update urls.py

--- kollabproject/kollabapp/urls.py
+++ kollabproject/kollabapp/urls.py
@@ -1,22 +1,21 @@
 
-
 from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('chatui/', views.chatui, name='home'),
-    path('', views.signup_view, name='signup'),
+    # path('chatui/', views.chatui, name='chatui'),
+    path("chatui/<int:workspace_id>/", views.chatui, name="chatui"),
+    path('', views.home, name='home'),
+    path('update_profile/', views.update_profile, name='update_profile'),
+    path('signup/', views.signup_view, name='signup'),
+    path('workspace/', views.workspace, name='workspace'),
+    path('profile/', views.profile, name='profile'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-]
-# from django.urls import path
-# from .views import SignUpView, AuthLoginView
-# from django.contrib.auth.views import LogoutView
-
-# urlpatterns = [
-#     path('signup/', SignUpView.as_view(), name='signup'),
-#     path('login/', AuthLoginView.as_view(), name='login'),
-#     path('logout/', LogoutView.as_view(), name='logout'),
-# ]
-
+    path('workspace/join/', views.join_workspace_manual, name='join_workspace_manual'),
+    path('workspace/<int:workspace_id>/add-member/', views.add_member_manual, name='add_member_manual'),
+    path("workspace/<int:workspace_id>/remove-member/", views.remove_member, name="remove_member"),
+    path("workspace/<int:workspace_id>/delete/", views.delete_workspace, name="delete_workspace"),
+] 
